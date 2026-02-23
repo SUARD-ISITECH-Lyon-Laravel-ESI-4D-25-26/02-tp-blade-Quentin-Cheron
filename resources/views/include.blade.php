1aa5f60b6ea8f0fa60eee2e01aa44f05a135bb39
quentin <quentin.cheron26200@hotmail.com>
2026-02-23 16:58:17 +0100

    finish all tasks

--- resources/views/include.blade.php
+++ resources/views/include.blade.php
@@ -21,6 +21,7 @@
                                 <tr class="bg-red-100">
                                     {{-- TÂCHE : incluez le fichier resources/views/includes/row.blade.php --}}
                                     {{-- en lui passant correctement la variable $user --}}
+                                    @include('rows', ['user' => $user])
                                 </tr>
                             @endforeach
                         </tbody>
